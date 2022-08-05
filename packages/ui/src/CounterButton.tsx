0f3e1170316d7199f659630324c7d88eea4cfa7f
jchen-vercel <jason.chen@vercel.com>
2022-08-05 00:33:15 -0400

    change on shared counterbutton and storefront app

--- packages/ui/src/CounterButton.tsx
+++ packages/ui/src/CounterButton.tsx
@@ -11,7 +11,7 @@ export const CounterButton = () => {
       }}
     >
       <p>
-        This5 is component is from <code>ui</code>
+        This6 is component is from <code>ui</code>
       </p>
       <p>
         <button type="button" onClick={() => setCount((c) => c + 1)}>
